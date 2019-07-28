06c679426cb3cf61c1593ad84a6e897c7c5bc276
bassamalasadi <bassamalasadi@gmail.com>
2019-07-28 21:23:01 +0300

    make it better

---
+++ part3/3.1_phonebook backend/build/precache-manifest.edb6ca8053d0c4efc23bc4b68b4c49a8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33a8d12f53af8f7b6ba5921a7e376b75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a99b34817e79f2bb8175",
+    "url": "/static/css/main.0efb9d65.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "a99b34817e79f2bb8175",
+    "url": "/static/js/main.10f93648.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ part3/3.1_phonebook backend/index.js
@@ -0,0 +1,70 @@
+const express = require('express')
+const app = express()
+const uuid = require('uuid')
+const persons = require('./persons')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const cors = require('cors')
+
+
+app.use(bodyParser.json())
+app.use(cors())
+
+morgan.token('data',(req, res) => {
+  if (req.method === 'POST') {
+    return (
+      JSON.stringify({
+          name: req.body.name,
+          number: req.body.number,
+      })
+  )}else{
+    return ' '
+}})
+
+app.use(morgan(':method :url :status :res[data-length] - :response-time ms :data'))
+
+app.post('/api/persons',(req,res) => {
+  const newUser = {
+    id: uuid.v4(),
+    name:req.body.name,
+    number:req.body.number
+  }
+  if (!newUser.name || !newUser.number) {
+    return res.status(400).json({msg: 'name or number missing'});
+  }
+  const found = persons.find(n=>n.name===newUser.name)
+  if(!found){
+    persons.push(newUser)
+    res.json(newUser)
+  }else{
+    res.status(400).json({msg:'name must be unique'});
+  }
+})
+
+app.get('/api/persons/:id',(req, res)=> {
+  const found = persons.some(p => p.id === parseInt(req.params.id));
+  if (found){
+    res.json(persons.filter(p => p.id === parseInt(req.params.id)));
+  }else{
+    res.status(400).json({msg: `no content with the id of ${req.params.id}`})
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  delperson = persons.filter(person => person.id !== id)
+  res.status(204).end()
+})
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+app.get('/info', (req, res) => {
+  const num = persons.length
+  const infoPage = `<p>Phonebook has info for ${num} people.</p> <p>${new Date}</p>`
+  res.send(infoPage)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
